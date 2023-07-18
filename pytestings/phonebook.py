472c9b62c54c5d985d6113976d143f25b3cfc1f7
radi@nexo.io <radi@nexo.io>
2023-07-19 00:15:51 +0300

    updates

--- pytestings/phonebook.py
+++ pytestings/phonebook.py
@@ -1,2 +1,9 @@
 class Phonebook:
-    pass
+    def __init__(self):
+        self.numbers = {}
+
+    def add(self, name, number):
+        self.numbers[name] = number
+
+    def lookup(self, name):
+        return self.numbers[name]
--- pytestings/test_phonebook.py
+++ pytestings/test_phonebook.py
@@ -1,11 +1,18 @@
 import unittest
- 
-from phonebook import Phonebook
 
-class Phonebook:
-    pass
+from phonebook import Phonebook
 
 
 class PhonebookTest(unittest.TestCase):
     def test_lookup_by_name(self):
         phonebook = Phonebook()
+        phonebook.add('Ivan', '123456')
+        number = phonebook.lookup('Ivan')
+
+        self.assertEqual(number, '123456')
+
+    def test_missing_name(self):
+        phonebook = Phonebook()
+
+        with self.assertRaises(KeyError):
+            phonebook.lookup('missing')
