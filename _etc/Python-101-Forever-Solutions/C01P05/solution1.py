5d848c01db5974135d4f3a22e163cd920dd5ca10
raddystr <raddy.radev@gmail.com>
2023-12-31 18:34:06 +0200

    updates

--- _etc/Python-101-Forever-Solutions/C01P05/solution1.py
+++
@@ -1,9 +0,0 @@
-def sum_matrix(m):
-    total = 0 
-    for array in range(len(m)):
-        total += sum(m[array])
-    return(total)
-
-print(sum_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == 45)
-print(sum_matrix([[0, 0, 0], [0, 0, 0], [0, 0, 0]]) == 0)
-print(sum_matrix([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]) == 55)
--- pytestings/phonebook.py
+++
@@ -1,9 +0,0 @@
-class Phonebook:
-    def __init__(self):
-        self.numbers = {}
-
-    def add(self, name, number):
-        self.numbers[name] = number
-
-    def lookup(self, name):
-        return self.numbers[name]
